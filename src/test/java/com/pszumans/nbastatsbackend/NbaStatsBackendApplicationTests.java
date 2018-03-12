06aaf5686a02a9876b18b7595b67dc49e658cf22
Szuman <pawel.szumanski@gmail.com>
2018-03-12 05:25:35 +0100

    minor addings

---
+++ src/test/java/com/pszumans/nbastatsbackend/NbaStatsBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pszumans.nbastatsbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NbaStatsBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
